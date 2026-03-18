6595b19d4b56724038e1a65fada56166bdc2f449
Keisuke Inoue <kskinoue@gmail.com>
2026-03-18 22:05:17 +0900

    main.cpp 実装

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "worker.h"
+#include "status.h"
+
 int main(int argc, char *argv[]) {
+    Status status;
+    Worker worker;
+
+    std::thread workerThread([&]() {
+        worker.doHeavyWork(status);
+    });
+
+    while (status.isRunning || status.progress < 100) {
+        std::cout << "progress: " << status.progress << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
+    std::cout << "progress: " << status.progress << std::endl;
+
+    workerThread.join();
     return 0;
 }
